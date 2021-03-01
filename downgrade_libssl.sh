5a6f2c6a3599e3622295328c0ff8e70c923624ee
Meep <meep@meeps.dk>
2021-03-01 21:24:05 +0100

    Update libssl downgrade script

--- downgrade_libssl.sh
+++ downgrade_libssl.sh
@@ -3,7 +3,7 @@ mkdir libssl
 cd libssl
 rm libssl-dev_1.1.1-1ubuntu2.1~18.04.7_amd64.deb
 rm libssl1.1_1.1.1-1ubuntu2.1~18.04.7_amd64.deb
-wget http://mirrors.kernel.org/ubuntu/pool/main/o/openssl/libssl-dev_1.1.1-1ubuntu2.1~18.04.7_amd64.deb
-wget http://mirrors.kernel.org/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.7_amd64.deb
-sudo dpkg -i libssl-dev_1.1.1-1ubuntu2.1~18.04.7_amd64.deb libssl1.1_1.1.1-1ubuntu2.1~18.04.7_amd64.deb
+wget http://launchpadlibrarian.net/523550952/libssl-dev_1.1.1-1ubuntu2.1~18.04.8_amd64.deb
+wget http://launchpadlibrarian.net/523550959/libssl1.1_1.1.1-1ubuntu2.1~18.04.8_amd64.deb
+sudo dpkg -i libssl-dev_1.1.1-1ubuntu2.1~18.04.8_amd64.deb libssl1.1_1.1.1-1ubuntu2.1~18.04.8_amd64.deb
 cd ..
